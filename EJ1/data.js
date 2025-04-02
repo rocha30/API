d8012ed27c94491aa63ddc6baed5629df0c00eb4
Mario Rocha <mariorocha@Marios-MacBook-Air.local>
2025-04-01 23:55:32 -0600

    Update de la API, conexion con la BD, primero querys.

--- EJ1/data.js
+++ EJ1/data.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const port = 3010; 
+
+/*
+const {Client} = requiere('pg');
+
+const obtenerUsuarios = async () => {
+    const client = new Client({
+        user: 'postgres', 
+        host: 'localhost', 
+        database: 'API', 
+        password: 'roc23501', 
+        port: 5432
+    }); 
+
+
+    await client.connect();
+
+    const res = await client.query('SELECT * FROM incidentes');
+    const result = res.rows;
+    await client.end();
+    return result;
+}
+
+obtenerUsuarios().then((result) => {
+    console.log(result);
+});
+*/
+
+
 
 
 // Middleware para parsear JSON en las solicitudes (explica que es un middleware)
@@ -37,6 +64,7 @@ app.post('/API/EJ1/post', (req, res) => {
     console.log("req", req);
     posts.push(newPost);
     res.send(posts);
+
 }
 );
 
---
+++ EJ1/db.js
@@ -0,0 +1,77 @@
+import pkg from 'pg';
+const { Client } = pkg;
+
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'API',
+    password: 'roc23501',
+    port: 5432,
+  });
+  
+  export default client;
+  
+
+
+await client.connect();
+
+const obtenerUsuarios = async () => {
+    
+
+    const res = await client.query('SELECT * FROM incidentes');
+    const result = res.rows;
+    
+    return result;
+}
+
+obtenerUsuarios().then((result) => {
+    console.log(result);
+});
+
+
+
+const agregarDatos = async () => {
+    
+    
+    const text = 'INSERT INTO incidentes(reporter, description, status) VALUES($1, $2, $3) RETURNING *'
+    const values = ['Joel Jaquez', 'Borre la base de datos', 'en proceso'];
+
+
+    const res = await client.query(text, values)
+    console.log(res.rows[0])
+    const result = res.rows;
+    
+    return result;
+
+};
+
+agregarDatos().then((result) => {
+    console.log(result);
+});
+
+
+
+const ObtenerUsuariosbyID = async () => {
+    
+
+
+    
+    const text = 'SELECT * FROM incidentes WHERE id = $1';
+    const values = [1];
+    const res = await client.query(text, values);
+    const result = res.rows;
+    
+    return result;
+
+}
+
+ObtenerUsuariosbyID().then((result)=> {
+    console.log(result);
+})
+
+
+
+
+
+//Aqui va la logica para la base de datos. 
